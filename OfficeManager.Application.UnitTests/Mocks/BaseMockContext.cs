7affa5639734f66e11c36edb493511dc34ab5fca
Rahul Patel <rpatel@brightree.com>
2022-08-23 19:02:45 +0530

    Application Unit Test Project added User Role work done

---
+++ OfficeManager.Application.UnitTests/Mocks/BaseMockContext.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OfficeManager.Application.Common.Mappings;
+
+namespace OfficeManager.Application.UnitTests.Mocks
+{
+    public class BaseMockContext
+    {
+        protected readonly IMapper _mapper;
+        public BaseMockContext()
+        {
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+       
+    }
+}
