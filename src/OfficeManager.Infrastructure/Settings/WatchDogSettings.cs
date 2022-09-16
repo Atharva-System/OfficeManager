0c3f8a5cbeda814da60271c474556ebec4993e02
Rahul Patel <rpatel@brightree.com>
2022-09-16 14:57:07 +0530

    watchdog implemented with postgresql

---
+++ src/OfficeManager.Infrastructure/Settings/WatchDogSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace OfficeManager.Infrastructure.Settings
+{
+    public class WatchDogSettings
+    {
+        public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
